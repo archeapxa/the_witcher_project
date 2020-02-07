1c3bff8a7f2ce591a297e86027565688f8599b4f
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-02-07 11:49:45 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    loop:true,
+    navigation: {
+        nextEl: '.arrow',
+      },
+    slidesPerView:1,
+    freeMode:true,
+    freeModeMomentum:true,
+    freeModeSticky:true,
+
+    breakpoints: {
+      540: {
+        slidesPerView: 2,
+        freeMode:true,
+        freeModeMomentum:true,
+        freeModeSticky:true,
+      }
+    }
+});
+
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header');
+menuButton.addEventListener('click', function() {
+  menuButton.classList.toggle('menu-button-active');
+  menu.classList.toggle('header-active');
+})
